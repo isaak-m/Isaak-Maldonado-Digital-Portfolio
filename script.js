25aa7813326d581b08cdcf77352c64af65b3a6aa
isaak-m <93939199+isaak-m@users.noreply.github.com>
2022-05-09 10:29:07 -0700

    burger attempt

--- script.js
+++ script.js
@@ -35,20 +35,13 @@ function validatePhoneEmail() {
     console.log(emailTest());
 }
 //burger idea
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.countrypages');
-    const countryPages = document.querySelectorAll('.countrypages div');
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        countryPages.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `countryPagesFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-            }
-        });
-        burger.classList.toggle('toggle');
-    });
+function burgerMenu() {//MAYBE TRY TO WRITE THE CSS FOR THE DROPDOWNCONTENT TO WORK PROPERLY
+    let pageLinks = document.getElementById('pageLinks');
+    if (pageLinks.style.display === 'flex') {
+        pageLinks.style.display = 'none';
+    } else {
+        pageLinks.style.display = 'flex';
+        pageLinks.style.flexDirection = 'column'
+
+    }
 }
-navSlide();
