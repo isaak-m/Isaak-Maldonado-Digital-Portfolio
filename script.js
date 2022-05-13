0c12e2eafb848347d88d91c1bc091bacacdd9d10
isaak-m <93939199+isaak-m@users.noreply.github.com>
2022-05-13 10:29:39 -0700

    contact validation attempt

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
 //Contact validation form
-function emailTest(form)  {
-    let emailLength = 0;
-    if (form.email_address.value.indexOf("@", 0) < 0) {
+function formValidation()  {
+    let email = document.getElementsByName("emailAddress").value;
+    const emailStr = String(email);
+    let phone = document.getElementsByName("phoneNumber").value;
+    const phoneStr = String(phone);
+    if ((emailStr.indexOf('@') === -1) && (phoneStr.length < 8)) {
+        alert("Not a valid e-mail address and phone number is too short (less than 9 characters)");
+        document.write(email)
+        return false;
+    } else if (emailStr.indexOf('@') === -1) {
         alert("Not a valid e-mail address!");
-        emailLength = 1;
+        return false;
+    } else if (phoneStr.length < 8) {
+        alert("Phone number is too short (less than 9 characters)");
+        return false;
     } else {
-        let strLength = document.myForm.email_address.value.length;
-        if(strLength < 6 ) {
-            alert("E-mail address is too short (less than 6 characters)");
-            emailLength = 1;}
+        return true;
     }
-    if(emailLength == 0) {
-        alert("This e-mail address could be valid and is at least six characters long");
-    }
-}
-function phoneTest(form)  {
-    let phoneLength = 0;
-    if (form.phone_number.value.indexOf(("(" && ")"), 0) < 0) {
-        alert("Not a valid phone number!");
-        phoneLength = 1;
-    } else {
-        let strLength = document.myForm.phone_number.value.length;
-        if(strLength < 8 ) {
-            alert("E-mail address is too short (less than 9 characters)");
-            phoneLength = 1;}
-    }
-    if(phoneLength == 0) {
-        alert("This phone number could be valid and is at least nine characters long");
-    }
-}
-function validatePhoneEmail() {
-    document.getElementById("validationForm").submit();
-    console.log(phoneTest());
-    console.log(emailTest());
 }
 //burger
 function burgerMenu() {
